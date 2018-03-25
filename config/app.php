a9715c31e8ceef01051113a41b5d5abb433e2449
aipopablo <pablo.93jeje@gmail.com>
2018-03-25 11:27:50 -0400

    commit con algunos cambios en el esquema de seguridad. referencia de cómo va a funcionar: https://medium.com/@yamidvo/tutorial-sistema-de-roles-y-permisos-en-laravel-entrust-a69a8efda3e2

--- config/app.php
+++ config/app.php
@@ -160,6 +160,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+        *Provider zizaco para el manejo de los usuarios.
+        */
+        Zizaco\Entrust\EntrustServiceProvider::class,
+
     ],
 
     /*
@@ -208,6 +213,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Entrust' => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
