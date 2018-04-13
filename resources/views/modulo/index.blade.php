ebe6e6f1ed8303d10b231258255b5000a4d2ea29
ale395 <alexis.fernandez.rc@gmail.com>
2018-04-13 11:24:43 -0400

    Desactivar botonoes si no se tienen permisos
    Se agregó la verificacion de permisos para Módulos en la vista index, para que desabilite los botones de crear, editar o eliminar si el usuario no tiene permiso para esas acciones.

--- resources/views/modulo/index.blade.php
+++ resources/views/modulo/index.blade.php
@@ -12,20 +12,34 @@
 		</thead>
 		<tbody>
 			<a href="{{ route('modulos.create') }}">
-				<button class="btn btn-success">Nuevo Registro</i></button>
+				@can('modulos.create')
+					<button class="btn btn-success">Nuevo Registro</i></button>
+				@else
+					<button class="btn btn-success" disabled>Nuevo Registro</i></button>
+				@endcan
 			</a>
 			@foreach($modulos as $modulo)
 				<tr>
 					<td>{{$modulo->modulo}}</td>
 					<td>{{$modulo->descripcion}}</td>
 					<td>
-						<a href="{{url('/modulos/'.$modulo->id.'/edit')}}"><button class="btn btn-warning btn-sm">
-							<i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar</button></a>
+						
+						<a href="{{url('/modulos/'.$modulo->id.'/edit')}}">
+							@can('modulos.edit')
+								<button class="btn btn-warning btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar</button>
+							@else
+								<button class="btn btn-warning btn-sm" disabled><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar</button>
+							@endcan
+						</a>
+
 						<form style="display: inline" method="POST" action="{{ route('modulos.destroy', $modulo->id) }}">
 							{!! csrf_field() !!}
 							{!! method_field('DELETE') !!}
-							<button class="btn btn-danger btn-sm">
-							<i class="fa fa-trash" aria-hidden="true"></i> Eliminar</button>
+							@can('modulos.destroy')
+								<button class="btn btn-danger btn-sm"><i class="fa fa-trash" aria-hidden="true"></i> Eliminar</button>
+							@else
+								<button class="btn btn-danger btn-sm" disabled><i class="fa fa-trash" aria-hidden="true"></i> Eliminar</button>
+							@endcan
 						</form>
 					</td>
 				</tr>
