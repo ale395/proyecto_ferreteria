1f53ddf273c72017bbce35fb231af2debf30010a
ale395 <alexis.fernandez.rc@gmail.com>
2018-04-12 20:31:09 -0400

    Ajustes de vistas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,151 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="images/favicon.ico" type="image/ico" />
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <title> {{ config('app.name') }} </title>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- Custom Theme Style -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+  </head>
 
-                    You are logged in!
-                </div>
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="#" class="site_title"><i class="fa fa-recycle"></i> <span> {{ config('app.name') }} </span></a>
             </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="images/user.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Bienvenido/a</span>
+                <h2>{{ Auth::user()->name }}</h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>Menú</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-cogs"></i> Parámetros Generales <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <!--<li><a href="#level1_1">Level One</a>-->
+                        <li><a>Formularios<span class="fa fa-chevron-down"></span></a>
+                          <ul class="nav child_menu">
+                            <li class="sub_menu"><a href="{{route('modulos.index')}}">Módulos</a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li><a>Reportes<span class="fa fa-chevron-down"></span></a>
+                          <ul class="nav child_menu">
+                            <li class="sub_menu"><a href="#">Ranking de ventas</a>
+                            </li>
+                          </ul>
+                        </li>
+                        <!--<li><a href="#level1_2">Level One</a>-->
+                        </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+            <!-- /sidebar menu -->
+
+            <!-- /menu footer buttons -->
+            <div class="sidebar-footer hidden-small">
+              <a data-toggle="tooltip" data-placement="top" title="Settings">
+                <span class="fa fa-cog" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+                <span class="fa fa-arrows-alt" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="Lock">
+                <span class="fa fa-eye-slash" aria-hidden="true"></span>
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+              </form>
+              
+              <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
+                <span class="fa fa-power-off" aria-hidden="true"></span>
+              </a>
+              
+            </div>
+            <!-- /menu footer buttons -->
+          </div>
         </div>
+
+        <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+            <nav>
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+
+              <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img src="images/user.jpg" alt="">{{ Auth::user()->name }}
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="fa fa-sign-out pull-right"></i>Cerrar Sesión</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- /top navigation -->
+
+        <!-- extendemos con todo lo que tenemos -->
+        
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <!-- top tiles -->
+          @yield('content')
+        </div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Sistema de Gestión para Ferretería
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
     </div>
-</div>
-@endsection
+
+    <main class="py-4">
+        @yield('login')
+    </main>
+
+    <!-- Custom Theme Scripts -->
+    <script src="{{asset('js/app.js')}}"></script>
+  
+  </body>
+</html>
--- resources/views/modulo/edit.blade.php
+++ resources/views/modulo/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('home2')
+@extends('home')
 
 @section('content')
 
