0cb1dc5afbcf909d25b667eb86c720e16643f20c
Ysosa <Yani_rsc@hotmail.com>
2018-08-19 21:07:47 -0400

    modificaciones posteriores al merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,10 @@
 
     <!-- Custom Theme Style -->
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css">
     
     
   </head>
@@ -72,8 +75,14 @@
                               <!--<li><a href="#level1_1">Level One</a>-->
                               <li><a>Formularios<span class="fa fa-chevron-down"></span></a>
                                 <ul class="nav child_menu">
-                                    <li class="sub_menu"><a href="#">Formulario 1</a>
-                                    </li>
+                                    @can('clasificacioncliente.index')
+                                      <li class="sub_menu"><a href="{{route('clasificacionclientes.index')}}">Tipos de Clientes</a>
+                                      </li>
+                                    @endcan
+                                    @can('clientes.index')
+                                      <li class="sub_menu"><a href="{{route('clientes.index')}}">Clientes</a>
+                                      </li>
+                                    @endcan
                                 </ul>
                               </li>
                               <li><a>Reportes<span class="fa fa-chevron-down"></span></a>
@@ -108,10 +117,6 @@
                         <!--<li><a href="#level1_1">Level One</a>-->
                         <li><a>Formularios<span class="fa fa-chevron-down"></span></a>
                           <ul class="nav child_menu">
-                            @can('conceptos.index')
-                              <li class="sub_menu"><a href="{{route('tconceptos.index')}}">Conceptos</a>
-                              </li>
-                            @endcan
                             @can('timbrados.index')
                               <li class="sub_menu"><a href="{{route('timbrados.index')}}">Timbrados</a>
                               </li>
@@ -140,18 +145,15 @@
                             @endcan
                             @can('rubros.index')
                               <li class="sub_menu"><a href="{{route('rubros.index')}}">Rubros</a>
+                              <li class="sub_menu"><a href="{{route('numeSeries.index')}}">Series</a>
                               </li>
                             @endcan
-                            @can('unidadmedidas.index')
-                              <li class="sub_menu"><a href="{{route('unidadmedidas.index')}}">Unidad de Medidas</a>
+                            @can('sucursales.index')
+                              <li class="sub_menu"><a href="{{route('sucursales.index')}}">Sucursales</a>
                               </li>
                             @endcan
-                            @can('conceptoajuste.index')
-                              <li class="sub_menu"><a href="{{route('conceptos.index')}}">Conceptos de Ajuste</a>
-                              </li>
-                            @endcan
-                            @can('clasificacioncliente.index')
-                              <li class="sub_menu"><a href="{{route('clasificacionclientes.index')}}">Clasificacion de Clientes</a>
+                            @can('bancos.index')
+                              <li class="sub_menu"><a href="{{route('bancos.index')}}">Bancos</a>
                               </li>
                             @endcan
                             @can('cajeros.index')
@@ -200,8 +202,26 @@
                               <!--<li><a href="#level1_1">Level One</a>-->
                               <li><a>Formularios<span class="fa fa-chevron-down"></span></a>
                                 <ul class="nav child_menu">
-                                    <li class="sub_menu"><a href="#">Formulario 1</a>
-                                    </li>
+                                    @can('familias.index')
+                                      <li class="sub_menu"><a href="{{route('familias.index')}}">Familias</a>
+                                      </li>
+                                    @endcan
+                                    @can('lineas.index')
+                                      <li class="sub_menu"><a href="{{route('lineas.index')}}">Lineas</a>
+                                      </li>
+                                    @endcan
+                                    @can('rubros.index')
+                                      <li class="sub_menu"><a href="{{route('rubros.index')}}">Rubros</a>
+                                      </li>
+                                    @endcan
+                                    @can('unidadmedidas.index')
+                                      <li class="sub_menu"><a href="{{route('unidadmedidas.index')}}">Unidad de Medidas</a>
+                                      </li>
+                                    @endcan
+                                    @can('conceptoajuste.index')
+                                      <li class="sub_menu"><a href="{{route('conceptos.index')}}">Conceptos de Ajuste</a>
+                                      </li>
+                                    @endcan
                                 </ul>
                               </li>
                               <li><a>Reportes<span class="fa fa-chevron-down"></span></a>
@@ -218,7 +238,10 @@
                               <!--<li><a href="#level1_1">Level One</a>-->
                               <li><a>Formularios<span class="fa fa-chevron-down"></span></a>
                                 <ul class="nav child_menu">
-                                    
+                                    @can('vendedores.index')
+                                      <li class="sub_menu"><a href="{{route('vendedores.index')}}">Vendedores</a>
+                                      </li>
+                                    @endcan
                                       <li class="sub_menu"><a href="{{route('listaPrecios.index')}}">Lista de Precios</a>
                                       </li>
                                     
@@ -327,16 +350,29 @@
     <script src="{{asset('js/app.js')}}"></script>
 
     <script src="{{ asset('assets/jquery/jquery.min.js') }}"></script>
+    
     <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
 
     <script src="{{ asset('assets/jquery/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js') }}"></script>
+
     <script src="{{ asset('assets/dataTables/js/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('assets/dataTables/js/dataTables.bootstrap.min.js') }}"></script>
     <script src="{{ asset('assets/validator/validator.js') }}"></script>
 
     <script src="{{ asset('assets/bootstrap/js/ie10-viewport-bug-workaround.js') }}"></script>
+
     <script type="text/javascript" src="{{ asset('assets/gentelella/smartresize.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/i18n/es.js"></script>
+
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/locales/bootstrap-datepicker.es.min.js"></script>
 
     <!-- Para los Script JavaScript necesarios para la utilización de AJAX con el DataTables-->
     @yield('ajax_datatables')
