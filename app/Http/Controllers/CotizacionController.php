473fbc9bc493a3a45b70095b4ff360caec012be9
Ysosa <Yani_rsc@hotmail.com>
2018-08-21 20:43:14 -0400

    cotizacion y deposito controller
    finalizacion de cotizacion y depositos controller

--- app/Http/Controllers/CotizacionController.php
+++ app/Http/Controllers/CotizacionController.php
@@ -6,5 +6,124 @@ use Illuminate\Http\Request;
 
 class CotizacionController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('cotizacion.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = [
+        'descripcion' => $request['descripcion']
+        ];
+
+        return Cotizacion::create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Cotizacion  $cotizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cotizacion $cotizacion)
+    {
+        //
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Cotizacion  $cotizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cotizacion $cotizacion)
+    {
+        dd($cotizacion);
+        return $cotizacion;
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Cotizacion  $cotizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cotizacion $cotizacion)
+    {
+        $cotizacion->descripcion = $request['cotizacion'];
+        
+        $cotizacion->update();
+
+        return $cotizacion;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Cotizacion  $cotizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Cotizacion::destroy($id);
+    }
+
+    //Función que retorna un JSON con todos los módulos para que los maneje AJAX del lado del servidor
+    public function apiCotizacion()
+    {
+        $permiso_editar = Auth::user()->can('cotizacion.edit');;
+        $permiso_eliminar = Auth::user()->can('cotizacion.destroy');;
+        $cotizacion = Cotizacion::all();
+
+        if ($permiso_editar) {
+            if ($permiso_eliminar) {
+                return Datatables::of($cotizacion)
+                ->addColumn('action', function($cotizacion){
+                    return '<a onclick="editForm('. $cotizacion->id .')" class="btn btn-warning btn-sm"><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                           '<a onclick="deleteData('. $cotizacion->id .')" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i> Eliminar</a>';
+                })->make(true);
+            } else {
+                return Datatables::of($cotizacion)
+                ->addColumn('action', function($cotizacion){
+                    return '<a onclick="editForm('. $cotizacion->id .')" class="btn btn-warning btn-sm"><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                           '<a class="btn btn-danger btn-sm" disabled><i class="fa fa-trash-o"></i> Eliminar</a>';
+                })->make(true);
+            }
+        } elseif ($permiso_eliminar) {
+            return Datatables::of($cotizacion)
+            ->addColumn('action', function($cotizacion){
+                return '<a class="btn btn-warning btn-sm" disabled><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                       '<a onclick="deleteData('. $cotizacion->id .')" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i> Eliminar</a>';
+            })->make(true);
+        } else {
+            return Datatables::of($cotizacion)
+            ->addColumn('action', function($cotizacion){
+                return '<a class="btn btn-warning btn-sm" disabled><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                       '<a class="btn btn-danger btn-sm" disabled><i class="fa fa-trash-o"></i> Eliminar</a>';
+            })->make(true);
+        }
+    }
 }
--- app/Http/Controllers/DepositoController.php
+++ app/Http/Controllers/DepositoController.php
@@ -6,5 +6,125 @@ use Illuminate\Http\Request;
 
 class DepositoController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('deposito.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = [
+        'descripcion' => $request['descripcion']
+        ];
+
+        return Deposito::create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Deposito  $cotizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Deposito $deposito)
+    {
+        //
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Deposito  $cotizacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Deposito $deposito)
+    {
+        dd($deposito);
+        return $deposito;
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Deposito  $deposito
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Deposito $deposito)
+    {
+        $deposito->descripcion = $request['deposito'];
+        
+        $deposito->update();
+
+        return $deposito;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Deposito  $deposito
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Deposito::destroy($id);
+    }
+
+    //Función que retorna un JSON con todos los módulos para que los maneje AJAX del lado del servidor
+    public function apiDeposito()
+    {
+        $permiso_editar = Auth::user()->can('deposito.edit');;
+        $permiso_eliminar = Auth::user()->can('deposito.destroy');;
+        $deposito = Deposito::all();
+
+        if ($permiso_editar) {
+            if ($permiso_eliminar) {
+                return Datatables::of($deposito)
+                ->addColumn('action', function($deposito){
+                    return '<a onclick="editForm('. $deposito->id .')" class="btn btn-warning btn-sm"><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                           '<a onclick="deleteData('. $deposito->id .')" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i> Eliminar</a>';
+                })->make(true);
+            } else {
+                return Datatables::of($deposito)
+                ->addColumn('action', function($deposito){
+                    return '<a onclick="editForm('. $deposito->id .')" class="btn btn-warning btn-sm"><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                           '<a class="btn btn-danger btn-sm" disabled><i class="fa fa-trash-o"></i> Eliminar</a>';
+                })->make(true);
+            }
+        } elseif ($permiso_eliminar) {
+            return Datatables::of($deposito)
+            ->addColumn('action', function($deposito){
+                return '<a class="btn btn-warning btn-sm" disabled><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                       '<a onclick="deleteData('. $deposito->id .')" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i> Eliminar</a>';
+            })->make(true);
+        } else {
+            return Datatables::of($deposito)
+            ->addColumn('action', function($deposito){
+                return '<a class="btn btn-warning btn-sm" disabled><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                       '<a class="btn btn-danger btn-sm" disabled><i class="fa fa-trash-o"></i> Eliminar</a>';
+            })->make(true);
+        }
+    }
 }
+
