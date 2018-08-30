d1f3ae160e0c094a6206bbdaa55926b10b55eb88
ale395 <alexis.fernandez.rc@gmail.com>
2018-08-30 12:21:54 -0400

    Tipos de Empleados Controller

--- app/Http/Controllers/TipoEmpleadoController.php
+++ app/Http/Controllers/TipoEmpleadoController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use App\TipoEmpleado;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Datatables;
+use Illuminate\Support\Facades\Auth;
 
 class TipoEmpleadoController extends Controller
 {
@@ -14,7 +17,7 @@ class TipoEmpleadoController extends Controller
      */
     public function index()
     {
-        //
+        return view('tipoempleado.index');
     }
 
     /**
@@ -35,7 +38,26 @@ class TipoEmpleadoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'codigo' => 'required|max:20|unique:tipos_empleados},codigo',
+            'nombre' => 'required|max:100'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            $errors =  json_decode($errors);
+
+            return response()->json(['errors' => $errors], 422); // Status code here
+        }
+
+        $data = [
+            'codigo' => $request['codigo'],
+            'nombre' => $request['nombre']
+        ];
+
+        return TipoEmpleado::create($data);
     }
 
     /**
@@ -55,9 +77,10 @@ class TipoEmpleadoController extends Controller
      * @param  \App\TipoEmpleado  $tipoEmpleado
      * @return \Illuminate\Http\Response
      */
-    public function edit(TipoEmpleado $tipoEmpleado)
+    public function edit($id)
     {
-        //
+        $tipo_empleado = TipoEmpleado::findOrFail($id);
+        return $tipo_empleado;
     }
 
     /**
@@ -67,9 +90,30 @@ class TipoEmpleadoController extends Controller
      * @param  \App\TipoEmpleado  $tipoEmpleado
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, TipoEmpleado $tipoEmpleado)
+    public function update(Request $request, $id)
     {
-        //
+        $tipo_empleado = TipoEmpleado::findOrFail($id);
+
+        $rules = [
+            'codigo' => 'required|max:20|unique:tipos_empleados,codigo,'.$tipo_empleado->id,
+            'nombre' => 'required|max:100'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            $errors =  json_decode($errors);
+
+            return response()->json(['errors' => $errors], 422); // Status code here
+        }
+
+        $tipo_empleado->codigo = $request['codigo'];
+        $tipo_empleado->nombre = $request['nombre'];
+        
+        $tipo_empleado->update();
+
+        return $tipo_empleado;
     }
 
     /**
@@ -78,8 +122,43 @@ class TipoEmpleadoController extends Controller
      * @param  \App\TipoEmpleado  $tipoEmpleado
      * @return \Illuminate\Http\Response
      */
-    public function destroy(TipoEmpleado $tipoEmpleado)
+    public function destroy($id)
     {
-        //
+        return TipoEmpleado::destroy($id);
+    }
+
+    public function apiTiposEmpleados()
+    {
+        $permiso_editar = Auth::user()->can('tiposEmpleados.edit');
+        $permiso_eliminar = Auth::user()->can('tiposEmpleados.destroy');
+        $tipos_empleados = TipoEmpleado::all();
+
+        if ($permiso_editar) {
+            if ($permiso_eliminar) {
+                return Datatables::of($tipos_empleados)
+                ->addColumn('action', function($tipos_empleados){
+                    return '<a onclick="editForm('. $tipos_empleados->id .')" class="btn btn-warning btn-sm"><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                           '<a onclick="deleteData('. $tipos_empleados->id .')" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i> Eliminar</a>';
+                })->make(true);
+            } else {
+                return Datatables::of($tipos_empleados)
+                ->addColumn('action', function($tipos_empleados){
+                    return '<a onclick="editForm('. $tipos_empleados->id .')" class="btn btn-warning btn-sm"><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                           '<a class="btn btn-danger btn-sm" disabled><i class="fa fa-trash-o"></i> Eliminar</a>';
+                })->make(true);
+            }
+        } elseif ($permiso_eliminar) {
+            return Datatables::of($tipos_empleados)
+            ->addColumn('action', function($tipos_empleados){
+                return '<a class="btn btn-warning btn-sm" disabled><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                       '<a onclick="deleteData('. $tipos_empleados->id .')" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i> Eliminar</a>';
+            })->make(true);
+        } else {
+            return Datatables::of($tipos_empleados)
+            ->addColumn('action', function($tipos_empleados){
+                return '<a class="btn btn-warning btn-sm" disabled><i class="fa fa-pencil-square-o"></i> Editar</a> ' .
+                       '<a class="btn btn-danger btn-sm" disabled><i class="fa fa-trash-o"></i> Eliminar</a>';
+            })->make(true);
+        }
     }
 }
