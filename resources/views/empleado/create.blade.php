fc9bdaf7df7d270cf49de581c8829c65ae198bef
ale395 <alexis.fernandez.rc@gmail.com>
2018-09-07 13:44:17 -0400

    Foto del Empleado - Create

--- resources/views/empleado/create.blade.php
+++ resources/views/empleado/create.blade.php
@@ -3,13 +3,15 @@
 @section('content')
 
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-xs-12">
         <form method="post" action="{{action('EmpleadoController@store')}}" class="form-horizontal form-label-left" data-toggle="validator">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h4>Nuevo Empleado</h4>
                 </div>
                 <div class="panel-body">
+                <div class="form-group">
+                <div class="col-xs-10">
                     <div class="form-group">
                         @if ($errors->any())
                             <div class="alert alert-danger alert-dismissible">
@@ -26,48 +28,49 @@
                     <input type="hidden" value="{{csrf_token()}}" name="_token" />
                     <input type="hidden" id="id" name="id">
                     <div class="form-group">
-                        <label for="nro_cedula" class="col-md-2 control-label">Nro CI*</label>
-                        <div class="col-md-3">
+                        <label for="nro_cedula" class="col-xs-2 control-label">Nro CI*</label>
+                        <div class="col-xs-3">
                             <input type="text" id="nro_cedula" name="nro_cedula" class="form-control number" value="{{old('nro_cedula')}}" placeholder="Nro de Cedula de Identidad" autofocus>
                         </div>
                     </div>
+                    
                     <div class="form-group">
-                        <label for="nombre" class="col-md-2 control-label">Nombre*</label>
-                        <div class="col-md-5">
+                        <label for="nombre" class="col-xs-2 control-label">Nombre*</label>
+                        <div class="col-xs-5">
                             <input type="text" id="nombre" name="nombre" class="form-control" value="{{old('nombre')}}" placeholder="Nombre(s) del Empleado">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="apellido" class="col-md-2 control-label">Apellido*</label>
-                        <div class="col-md-5">
+                        <label for="apellido" class="col-xs-2 control-label">Apellido*</label>
+                        <div class="col-xs-5">
                             <input type="text" id="apellido" name="apellido" class="form-control" value="{{old('apellido')}}" placeholder="Apellido(s) del Empleado">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="direccion" class="col-md-2 control-label">Dirección*</label>
-                        <div class="col-md-5">
+                        <label for="direccion" class="col-xs-2 control-label">Dirección*</label>
+                        <div class="col-xs-5">
                             <input type="text" id="direccion" name="direccion" class="form-control" value="{{old('direccion')}}" placeholder="Domicilio del Empleado">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="correo_electronico" class="col-md-2 control-label">Correo Electrónico*</label>
-                        <div class="col-md-5">
+                        <label for="correo_electronico" class="col-xs-2 control-label">Correo Elect.*</label>
+                        <div class="col-xs-5">
                             <input type="text" id="correo_electronico" name="correo_electronico" class="form-control" value="{{old('correo_electronico')}}" placeholder="empleado@email.com">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="telefono_celular" class="col-md-2 control-label">Tel. Celular*</label>
-                        <div class="col-md-4">
+                        <label for="telefono_celular" class="col-xs-2 control-label">Tel. Celular*</label>
+                        <div class="col-xs-4">
                             <input type="text" id="telefono_celular" name="telefono_celular" class="form-control" value="{{old('telefono_celular')}}" placeholder="(0999) 999-999" data-inputmask="'mask' : '(0999) 999-999'">
                         </div>
-                        <label for="fecha_nacimiento" class="col-md-2 control-label">Fecha Nacimiento*</label>
-                        <div class="col-md-3">
+                        <label for="fecha_nacimiento" class="col-xs-2 control-label">Fecha Nac.*</label>
+                        <div class="col-xs-2">
                             <input type="text" id="fecha_nacimiento" name="fecha_nacimiento" class="form-control dpfechanacimiento" value="{{old('fecha_nacimiento')}}" placeholder="dd/mm/aaaa" data-inputmask="'mask': '99/99/9999'">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="tipo_empleado" class="col-md-2 control-label">Tipo de Empleado*</label>
-                        <div class="col-md-5">
+                        <label for="tipo_empleado" class="col-xs-2 control-label">Tipo Empleado*</label>
+                        <div class="col-xs-5">
                             <select name="tipos_empleados[]" id="select2-tipos-empleados" class="form-control" style="width: 100%" multiple="multiple">
                                 <option></option>
                                 @foreach($tipos_empleados as $id => $tipo_empleado)
@@ -76,12 +79,24 @@
                             </select>
                         </div>
                     </div>
-                    <div class="modal-footer">
-                            <button type="submit" class="btn btn-primary btn-save">Guardar</button>
-                            <a href="{{route('empleados.index')}}" type="button" class="btn btn-default">Cancelar</a>
-                        </div>
+
+                </div><!--PRUEBAAAA-->
+
+                <div class="col-xs-2">
+                    <img src="{{ URL::to('/') }}/images/user.jpg" alt="..." class="img-responsive" width="100" height="100">
+                    <h5></h5>
+                    <button class="btn btn-default">Cambiar Foto</button>
                 </div>
+                    
             </div>
+            
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary btn-save">Guardar</button>
+                <a href="{{route('empleados.index')}}" type="button" class="btn btn-default">Cancelar</a>
+            </div>
+            
+            </div>
+        </div>
         </form>
     </div>
 </div>
