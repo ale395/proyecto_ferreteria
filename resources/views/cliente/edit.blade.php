13c9ce86f0e60154b071938584bc1f7cbdd065dc
ale395 <alexis.fernandez.rc@gmail.com>
2018-09-27 14:43:27 -0400

    Edit Cliente
    Tipo de Persona y funcionalidad con javascript para mostrar u ocultar campos según el tipo de persona correspondiente.

--- resources/views/cliente/edit.blade.php
+++ resources/views/cliente/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
-        <form method="post" action="{{action('ClienteController@update', $cliente->id)}}" class="form-horizontal" data-toggle="validator">
+        <form id="form-edit-cliente" method="post" action="{{action('ClienteController@update', $cliente->id)}}" class="form-horizontal" data-toggle="validator">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h4>Editar datos del cliente</h4>
@@ -28,32 +28,64 @@
                     <input name="_method" type="hidden" value="PATCH">
                     <input type="hidden" value="{{csrf_token()}}" name="_token" />
                     <input type="hidden" id="id" name="id">
+
                     <div class="form-group">
-                        <label for="nombre" class="col-md-2 control-label">Nombre *</label>
+                        <label class="control-label col-md-2 col-sm-2 col-xs-12">Tipo persona</label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          	<div id="tipo_persona" class="btn-group" data-toggle="buttons">
+                            	@if($cliente->getTipoPersona() == 'F')
+	                            	<label class="btn btn-default active" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+	                              	<input id="radioPersonaFisica" type="radio" name="tipo_persona" value="F" checked onchange="validarTipoPersona()" onfocus="personaFisicaAction()" autofocus> &nbsp; Física &nbsp;
+	                            	</label>
+	                            	<label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+	                              	<input type="radio" name="tipo_persona" value="J" onchange="validarTipoPersona()"> Jurídica
+	                            	</label>
+                            	@else
+                            		<label class="btn btn-default" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+	                              	<input type="radio" name="tipo_persona" value="F" onchange="validarTipoPersona()"> &nbsp; Física &nbsp;
+	                            	</label>
+	                            	<label class="btn btn-primary active" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+	                              	<input id="radioPersonaJuridica" type="radio" name="tipo_persona" value="J" checked onchange="validarTipoPersona()" onfocus="personaJuridicaAction()" autofocus> Jurídica
+	                            	</label>
+                            	@endif
+                          	</div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div id="divNombre" class="form-group">
+                        <label for="nombre" class="col-md-2 control-label">Nombre</label>
                         <div class="col-md-9">
-                            <input type="text" id="nombre" name="nombre" class="form-control" value="{{old('nombre',$cliente->nombre)}}">
+                            <input type="text" id="nombre" name="nombre" class="form-control" value="{{old('nombre',$cliente->getNombre())}}">
                             <span class="help-block with-errors"></span>
                         </div>
                     </div>
 
-                    <div class="form-group">
+                    <div id="divApellido" class="form-group">
                         <label for="apellido" class="col-md-2 control-label">Apellido</label>
                         <div class="col-md-9">
-                            <input type="text" id="apellido" name="apellido" class="form-control" value="{{old('apellido',$cliente->apellido)}}">
+                            <input type="text" id="apellido" name="apellido" class="form-control" value="{{old('apellido',$cliente->getApellido())}}">
+                            <span class="help-block with-errors"></span>
+                        </div>
+                    </div>
+
+                    <div id="divRazonSocial" class="form-group">
+                        <label for="razon_social" class="col-md-2 control-label">Razón Social</label>
+                        <div class="col-md-9">
+                            <input type="text" id="razon_social" name="razon_social" class="form-control" value="{{old('razon_social',$cliente->getRazonSocial())}}">
                             <span class="help-block with-errors"></span>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="nro_cedula" class="col-md-2 control-label">Nro Cédula</label>
-                        <div class="col-md-4">
-                            <input type="number" id="nro_cedula" name="nro_cedula" class="form-control" value="{{old('nro_cedula',$cliente->nro_cedula)}}">
+                        <label for="ruc" class="col-md-2 control-label">RUC</label>
+                        <div class="col-md-3">
+                            <input type="text" id="ruc" name="ruc" class="form-control" value="{{old('ruc',$cliente->getRuc())}}">
                             <span class="help-block with-errors"></span>
                         </div>
-
-                        <label for="ruc" class="col-md-1 control-label">RUC</label>
-                        <div class="col-md-4">
-                            <input type="text" id="ruc" name="ruc" class="form-control" value="{{old('ruc',$cliente->ruc)}}">
+                        <label id="labelNroCedula" for="nro_cedula" class="col-md-3 control-label">Nro Cédula</label>
+                        <div id="divNroCedula" class="col-md-3">
+                            <input type="text" id="nro_cedula" name="nro_cedula" class="form-control" value="{{old('nro_cedula',$cliente->getNroCedula())}}">
                             <span class="help-block with-errors"></span>
                         </div>
                     </div>
@@ -67,21 +99,29 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="telefono_celular" class="col-md-2 control-label">Nro Teléfono</label>
-                        <div class="col-md-4">
+                        <label for="telefono_celular" class="col-md-2 control-label">Teléfono Celular</label>
+                        <div class="col-md-3">
                             <input type="text" id="telefono_celular" name="telefono_celular" class="form-control" value="{{old('telefono_celular',$cliente->getTelefonoCelular())}}">
                             <span class="help-block with-errors"></span>
                         </div>
 
-                        <label for="correo_electronico" class="col-md-1 control-label">Email</label>
+                        <label for="correo_electronico" class="col-md-2 control-label">Email</label>
                         <div class="col-md-4">
-                            <input type="email" id="correo_electronico" name="correo_electronico" class="form-control">
+                            <input type="email" id="correo_electronico" name="correo_electronico" value="{{old('correo_electronico',$cliente->getCorreoElectronico())}}" class="form-control">
+                            <span class="help-block with-errors"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="telefono_linea_baja" class="col-md-2 control-label">Teléfono Línea Baja</label>
+                        <div class="col-md-3">
+                            <input type="text" id="telefono_linea_baja" name="telefono_linea_baja" class="form-control" value="{{old('telefono_linea_baja',$cliente->getTelefonoLineaBaja())}}">
                             <span class="help-block with-errors"></span>
                         </div>
                     </div>
                     
                     <div class="form-group">
-                        <label for="tipo_cliente_id" class="control-label col-md-2">Tipo de Cliente *</label>
+                        <label for="tipo_cliente_id" class="control-label col-md-2">Tipo de Cliente</label>
                         <div class="col-md-4">
                             <select name="tipo_cliente_id" id="select2-tipos" class="form-control" style="width: 100%">
                                 <option></option>
@@ -91,7 +131,7 @@
                             </select>
                         </div>
 
-                        <label for="zona_id" class="control-label col-md-1">Zona *</label>
+                        <label for="zona_id" class="control-label col-md-1">Zona</label>
                         <div class="col-md-4">
                             <select name="zona_id" id="select2-zonas" class="form-control" style="width: 100%">
                                 <option></option>
@@ -106,7 +146,11 @@
                         <label class="control-label col-md-2">Activo *</label>
                         <div class="col-md-1 custom-control custom-checkbox">
                             <input type="hidden" name="activo" value="false">
-                            <input id="activo" type="checkbox" class="custom-control-input" name="activo" value="true">
+                            @if($cliente->getActivo())
+                            	<input id="activo" type="checkbox" class="custom-control-input" name="activo" value="true" checked="checked">
+                            @else
+                            	<input id="activo" type="checkbox" class="custom-control-input" name="activo" value="true">
+                            @endif
                         </div>
                     </div>
 
@@ -122,27 +166,55 @@
 @endsection
 
 @section('otros_scripts')
-  <script type="text/javascript">
+<script type="text/javascript">
+
+	function validarTipoPersona(){
+		var tipo_persona = $("input[name='tipo_persona']:checked").val();
+		
+		if (tipo_persona == 'J') {
+			personaJuridicaAction();
+		} else if(tipo_persona == 'F'){
+			personaFisicaAction();
+		}
+	};
+
+	function personaFisicaAction() {
+		$("#radioPersonaFisica").prop('checked', true);
+		$('#divRazonSocial').hide();
+		$('#divNombre').show();
+		$('#divApellido').show();
+		$('#labelNroCedula').show();
+		$('#divNroCedula').show();
+	}
+
+	function personaJuridicaAction() {
+		$("#radioPersonaJuridica").prop('checked', true);
+		$('#divRazonSocial').show();
+		$('#divNombre').hide();
+		$('#divApellido').hide();
+		$('#labelNroCedula').hide();
+		$('#divNroCedula').hide();
+	}
+</script>
+<script type="text/javascript">
     $(document).ready(function(){
         $('#select2-zonas').select2({
                 placeholder : 'Seleccione una de las opciones',
                 tags: false,
                 width: 'resolve',
-                dropdownParent: $('#modal-form'),
                 language: "es"
             });
         });
-  </script>
+</script>
 
-  <script type="text/javascript">
+<script type="text/javascript">
     $(document).ready(function(){
             $('#select2-tipos').select2({
                 placeholder : 'Seleccione una de las opciones',
                 tags: false,
                 width: 'resolve',
-                dropdownParent: $('#modal-form'),
                 language: "es"
             });
         });
-  </script>
+</script>
 @endsection
