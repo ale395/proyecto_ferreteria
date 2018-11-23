5ca378a40de28d6cf74ad2734714c703f19122d7
Ysosa <Yani_rsc@hotmail.com>
2018-11-22 23:56:33 -0300

    agregar ruta de concepto ajuste
    nuevo cambio

--- routes/web.php
+++ routes/web.php
@@ -150,6 +150,7 @@ Route::middleware(['auth'])->group(function() {
 	//rutas para modelo 'conceptoajuste'
 	Route::resource('conceptos', 'ConceptoAjusteController');
 	Route::get('api/conceptosajuste', 'ConceptoAjusteController@apiConceptosAjuste')->name('api.conceptos');
+	Route::get('api/conceptosAjustes/buscador', 'ConceptoAjusteController@apiConceptosAjustesBuscador')->name('api.conceptosAjustes.buscador');
 
 	//rutas para modelo 'conceptos_caja'
 	Route::resource('conceptocaja', 'ConceptoCajaController');
