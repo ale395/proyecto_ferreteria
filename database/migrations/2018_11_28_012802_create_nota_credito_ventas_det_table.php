b6c2f1a092c93f915d3c7c5cc396ecf25e3f58fe
ale395 <alexis.fernandez.rc@gmail.com>
2018-11-27 23:12:47 -0300

    Nota Credito. Migracion y modelo.

---
+++ database/migrations/2018_11_28_012802_create_nota_credito_ventas_det_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotaCreditoVentasDetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nota_credito_ventas_det', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('nota_credito_cab_id')->unsigned();
+            $table->integer('articulo_id')->unsigned();
+            $table->decimal('cantidad', 14, 2);
+            $table->decimal('precio_unitario', 14, 2);
+            $table->integer('porcentaje_descuento')->unsigned();
+            $table->decimal('monto_descuento', 14, 2);
+            $table->integer('porcentaje_iva')->unsigned();
+            $table->decimal('monto_exenta', 14, 2);
+            $table->decimal('monto_gravada', 14, 2);
+            $table->decimal('monto_iva', 14, 2);
+            $table->decimal('monto_total', 14, 2);
+            $table->timestamps();
+
+            $table->foreign('articulo_id')->references('id')->on('articulos');
+            $table->foreign('nota_credito_cab_id')->references('id')->on('nota_credito_ventas_cab');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nota_credito_ventas_det');
+    }
+}
